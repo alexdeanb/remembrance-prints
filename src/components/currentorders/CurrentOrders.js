49e113c6444d25db160f61c4ffcb0a9ad2e31955
Alex Bishop <alexb98@gmail.com>
2023-04-28 14:13:43 -0500

    Current Orders table made

--- src/components/currentorders/CurrentOrders.js
+++ src/components/currentorders/CurrentOrders.js
@@ -1,8 +1,53 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Order } from "./Order";
 
 
 
 export const CurrentOrders = () => {
     
     const [orders, setOrders] = useState([])
-}
+
+    useEffect(
+        () => {
+          fetch("http://localhost:8088/orders")
+            .then((response) => response.json())
+            .then((orderArray) => {
+              setOrders(orderArray);
+            });
+        },
+        [] // When this array is empty, you are observing initial component state
+      );
+
+
+        return<>
+        <div>
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Customer</TableCell>
+                        <TableCell>Location</TableCell>
+                        <TableCell>Case Number</TableCell>
+                        <TableCell>Date Ordered</TableCell>
+                        <TableCell>Date Needed</TableCell>
+                        <TableCell>Designer</TableCell>
+                    </TableRow>
+                    {orders.map((order) => (
+                        <TableRow key={`order--${order.id}`}>
+                            <Order ticketObject={order} />
+                        </TableRow>
+
+
+                    ))}
+                </TableHead>
+            </Table>
+        </TableContainer>
+
+        </div>
+        </>
+
+
+
+
+    }
---
+++ src/components/currentorders/Order.js
@@ -0,0 +1,60 @@
+import { TableCell } from "@mui/material"
+import { useState, useEffect } from "react"
+
+
+export const Order = ({ticketObject}) => {
+    
+    const localPrintsUser = localStorage.getItem("prints_user");
+    const printsUserObject = JSON.parse(localPrintsUser);
+
+    const [locations, setLocations] = useState([])
+    const [users, setUsers] = useState([])
+    
+
+    useEffect(
+        () => {
+          fetch("http://localhost:8088/users")
+            .then((response) => response.json())
+            .then((userArray) => {
+              setUsers(userArray);
+            });
+
+            fetch("http://localhost:8088/locations")
+            .then((response) => response.json())
+            .then((locationsArray) => {
+              setLocations(locationsArray);
+            });
+
+        },
+        [] // When this array is empty, you are observing initial component state
+      );
+
+
+      const matchingDesigner = users.find((user) => user.id === ticketObject.designerId)
+
+
+
+
+    const hasDesigner = () => {
+        if(ticketObject.designerId === 0){
+            return "No Designer"
+        } else{
+            return matchingDesigner?.name
+        }
+    }
+
+
+    const matchingCustomer = users.find((user) => user.id === ticketObject.customerId)
+    const matchingLocation = locations.find((location) => location.id === ticketObject.locationId)
+
+
+
+    return <>
+    <TableCell>{matchingCustomer?.name}</TableCell>
+    <TableCell>{matchingLocation?.name}</TableCell>
+    <TableCell>{ticketObject?.caseNumber}</TableCell>
+    <TableCell>{ticketObject?.dateOrdered}</TableCell>
+    <TableCell>{ticketObject?.dateNeeded}</TableCell>
+    <TableCell>{hasDesigner()}</TableCell>
+    </>
+}
