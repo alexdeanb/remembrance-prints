d6f37915125b7b9fce4e015204c279034e642728
Alex Bishop <alexb98@gmail.com>
2023-05-05 09:17:49 -0500

    Dashboard updated

---
+++ src/components/home/ActiveByLocation.js
@@ -0,0 +1,55 @@
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+  
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+
+
+export const ActiveByLocation = ({orders}) => {
+
+     const options = {
+
+        }
+
+
+
+    const labels = ["Harper Mount", "West Harper", "Alexandria", "Ridgelook", "Henderboyville", "Mole & Gary", "Autumnfield"]
+
+    const HarperMountOrders = orders.filter((order) => order.locationId === 1).length
+    const WestHarperOrders = orders.filter((order) => order.locationId === 2).length
+    const AlexandriaOrders = orders.filter((order) => order.locationId === 3).length
+    const RidgelookOrders = orders.filter((order) => order.locationId === 4).length
+    const HenderboyvilleOrders = orders.filter((order) => order.locationId === 5).length
+    const MoleAndGaryOrders = orders.filter((order) => order.locationId === 6).length
+    const AutumnfieldOrders = orders.filter((order) => order.locationId === 7).length
+
+    const data = {
+    labels: labels,
+    datasets: [{
+        label: 'Location Orders',
+        data: [HarperMountOrders, WestHarperOrders, AlexandriaOrders, RidgelookOrders, HenderboyvilleOrders, MoleAndGaryOrders, AutumnfieldOrders],
+        fill: false,
+        backgroundColor: '#7e57c2',
+        tension: 0.1
+    }]
+};
+
+
+     return <Bar options={options} data={data} />;
+}
---
+++ src/components/home/Home.js
@@ -0,0 +1,40 @@
+import { Card, CardActions, CardContent } from "@mui/material"
+import { useEffect, useState } from "react";
+import { MainItem } from "./MainItem";
+import { ActiveByLocation } from "./ActiveByLocation";
+import { Container } from "@mui/system";
+
+
+export const Home = () => {
+
+
+    const [orders, setOrders] = useState([]);
+
+    useEffect(
+        () =>{
+        fetch(`http://localhost:8088/orders`)
+        .then(response => response.json())
+        .then((orderArray) => {
+            setOrders(orderArray)
+        })
+        },
+        []
+    )
+
+
+
+
+    return <>
+        <Card sx={{width: 300, mx: "auto"}}>
+            <CardContent>
+                <MainItem orders={orders}></MainItem>
+            </CardContent>
+        </Card>
+        <Card sx={{width: 300, mx: "auto"}}>
+            <CardContent>
+                <ActiveByLocation orders={orders} />
+            </CardContent>
+        </Card>
+    
+    </>
+}
